3382f1dfbd81020ebb42c5114a700145450cd755
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-05-28 18:51:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const go = require('gojs');
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
