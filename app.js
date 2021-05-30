a5119ecbf6e9bd38a9317891bff0bd2a3f00d2fb
amar-8 <aalhaw@gmail.com>
2021-05-30 19:24:29 +0200

    add save and check function for Dates

--- app.js
+++ app.js
@@ -25,23 +25,23 @@ app.get('/myapp', function(req, res) {
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
