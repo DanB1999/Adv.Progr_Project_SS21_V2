8ce88d963bdd9b678bd1da32e96a4ce86768c3b7
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-07-04 18:22:36 +0200

    data persistence (first try)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+/*
+var path = require('path');
 
+module.exports = (function () {
+  return path.dirname(require.main.filename || process.mainModule.filename);
+})();
+var root = require('root'); // In root will be absolute path to your application
+*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', function (req, res) {
+    let sampleFile;
+    let uploadPath;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        res.status(400).send('No files were uploaded.');
+        return;
+    }
+
+    console.log('req.files >>>', req.files); // eslint-disable-line
+
+    sampleFile = req.files.sampleFile;
+
+    uploadPath = __dirname + '/uploads/' + sampleFile.name;
+
+    console.log('uploadPath:' + uploadPath);
+    sampleFile.mv(uploadPath, function (err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        res.send('File uploaded to ' + uploadPath);
+    });
 });
 
 module.exports = router;
